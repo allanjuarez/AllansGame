9d1a8fa85b30284242f5fc9c27e60458c489f838
System Administrator <root@79p-MissionBit.local>
2019-06-27 09:27:41 -0700

    whole project

---
+++ CodePractice/Assets/Scripts/LevelOneControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneControls : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    Animator anim;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 targetVelocity = new Vector2(moveHorizontal * 10f, moveVertical * 10f);
+        rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
+
+        if (System.Math.Abs(rb2d.velocity.x) > 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
